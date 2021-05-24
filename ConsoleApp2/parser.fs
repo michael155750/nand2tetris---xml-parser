12e67a6dbcceeb57ce5701bf752f67ddf4cc961b
מיכאל <michael155750@gmail.com>
2021-05-24 20:08:35 +0300

    classParse finish

--- ConsoleApp2/parser.fs
+++ ConsoleApp2/parser.fs
@@ -6,36 +6,30 @@ open System.Xml.Linq
 
 open System.IO
 
-let private subroutineDec (rootEl:XElement) (en:Collections.Generic.IEnumerator<XElement>) = null
+let private subroutineDec (en:byref<Collections.Generic.IEnumerator<XElement>>)= null
 
-let private classVarDec (rootEl:XElement) (en:Collections.Generic.IEnumerator<XElement>) = null
+let private classVarDec (en:byref<Collections.Generic.IEnumerator<XElement>>)= null
 
-let private classParse (rootEl:XElement) (en:Collections.Generic.IEnumerator<XElement>) =
+let private classParse (rootEl:XElement) (en:byref<Collections.Generic.IEnumerator<XElement>>) =
     
-    let mutable classVarDecF = false
-    let mutable subroutineDecF = false
+    
     let mutable i = 0
     let mutable classEl = XElement(XName.Get("class"))
-    ///////////////
+   
     
     while en.MoveNext() do
         if i < 3 then
             classEl.Add(en.Current)
-        elif i = 3 then
+        elif i >= 3 then
             while (en.Current.Value = "static") || (en.Current.Value = "field") do
-                classEl.Add(classVarDec rootEl en)
-            while (en.Current.Value = "constractor") || (en.Current.Value = "static") 
-            || (en.Current.Value = "method") do
-                classEl.Add(subroutineDec rootEl en)
+                classEl.Add(classVarDec &en)
+            while (en.Current.Value = "constructor") || (en.Current.Value = "function") || (en.Current.Value = "method") do
+                classEl.Add(subroutineDec &en)
+            else
+                classEl.Add(en.Current)
         i <- i + 1
-    classEl
-    ////////////////
-    for el in rootEl.Elements() do
-        if i < 3 then
-            classEl.Add(el)
-        elif i = 3 then
-            if el.Value = "static" || el.Value = "field" then
-        i <- i + 1
+    
+    
 
 let parserMain path = 
     let filesList = Directory.GetFiles(path,"*T.xml")
@@ -45,8 +39,8 @@ let parserMain path =
         if File.Exists(path2) then
             File.Delete(path2)
         let mutable el = XElement.Load(f)
-        el.Name <- XName.Get("class")
-        /////////////////
-        let en =  el.Elements().GetEnumerator()
-        //////////////////
-        classParse el en
+        //el.Name <- XName.Get("class")
+        
+        let mutable en =  el.Elements().GetEnumerator()
+        
+        classParse el &en
