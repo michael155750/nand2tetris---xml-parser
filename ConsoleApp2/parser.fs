21065493b5fa6b6361cb47dccdcf2540aa188d19
zadoknet <zadoknet@gmail.com>
2021-05-25 22:57:23 +0300

    Netanel Changes

--- ConsoleApp2/parser.fs
+++ ConsoleApp2/parser.fs
@@ -6,7 +6,48 @@ open System.Xml.Linq
 
 open System.IO
 
-let private subroutineDec (en:byref<Collections.Generic.IEnumerator<XElement>>)= null
+let private varDec (en:byref<Collections.Generic.IEnumerator<XElement>>)=
+    let varDecEl=new XElement(XName.Get("varDec"))
+    varDecEl.Add(en.Current);
+    en.MoveNext|>ignore
+    while not(en.Current.Value.Replace(" ","").Equals(";")) do
+        varDecEl.Add(en.Current)
+        en.MoveNext()|>ignore
+    varDecEl.Add(en.Current)
+    varDecEl
+
+let private subroutineBody (en:byref<Collections.Generic.IEnumerator<XElement>>)=
+    let subroutineBodyEl=new XElement(XName.Get("subroutineBody"))
+    subroutineBodyEl.Add(en.Current) //add {
+    en.MoveNext|>ignore
+    while (en.Current.Value.Replace(" ","").Equals("static"))  do
+        subroutineBodyEl.Add(varDec &en)
+    subroutineBodyEl.Add(statements &en)
+    subroutineBodyEl
+     
+let private parameterList (en:byref<Collections.Generic.IEnumerator<XElement>>)=
+    let parameterListEl=new XElement(XName.Get("parameterList"))
+    while not (en.Current.Value.Replace(" ","").Equals(")")) do
+        parameterListEl.Add(en.Current)
+        en.MoveNext()|>ignore
+    0
+
+let private subroutineDec (en:byref<Collections.Generic.IEnumerator<XElement>>)=
+    
+    let mutable subEl = XElement(XName.Get("subroutineDec"))
+    subEl.Add(en.Current)//add the word function/method /constructor
+    en.MoveNext()|>ignore
+    subEl.Add(en.Current)//add void/type
+    en.MoveNext()|>ignore
+    subEl.Add(en.Current)//add name of function
+    en.MoveNext()|>ignore
+    subEl.Add(en.Current)//add '('
+    en.MoveNext()|>ignore
+    subEl.Add(parameterList &en)//add parameterList
+    subEl.Add(en.Current)//add ')'
+    en.MoveNext()|>ignore
+    subEl.Add(subroutineBody &en)
+    0
 
 let private classVarDec (en:byref<Collections.Generic.IEnumerator<XElement>>)= 
     let mutable varEl=new XElement(XName.Get("classVarDec"))
@@ -16,7 +57,7 @@ let private classVarDec (en:byref<Collections.Generic.IEnumerator<XElement>>)=
     varEl.Add(en.Current)
     en.MoveNext()|>ignore
     varEl
-    
+
 
 let private classParse (rootEl:XElement) (en:byref<Collections.Generic.IEnumerator<XElement>>) =
     
